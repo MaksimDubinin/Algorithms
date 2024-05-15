e63b925f084b89445a66625ac9c12d4a590d12e3
Максим Дубинин <maksimdubinin@MacBook-Pro-Maksim.local>
2024-05-15 23:58:31 +0300

    Добавил сортировку пузырьком

--- app.js
+++ app.js
@@ -46,11 +46,64 @@ const algorithmsApp = (input) => {
     const sliderChild2 = document.createElement('article');
     const sliderChild3 = document.createElement('article');
 
-    sliderChild1.setAttribute('id', 's1');
-    
-    sliderChild2.setAttribute('id', 's2');
+    const divTitleBubble = document.createElement('div');
+    divTitleBubble.setAttribute('class', 'divsTitle');
 
-    sliderChild3.setAttribute('id', 's3');
+    const bubbleTextName = document.createElement('p');
+    bubbleTextName.setAttribute('class', 'algName');
+    bubbleTextName.innerHTML = "Сортировка Пузырьком";
+    divTitleBubble.append(bubbleTextName);
+    const realizationText = document.createElement('p');
+    realizationText.setAttribute('id', 'realizationText');
+    realizationText.innerHTML = "Реализация на JS";
+    divTitleBubble.append(realizationText);
+    sliderChild1.append(divTitleBubble);
+    
+    const divContentBubble = document.createElement('div');
+    divContentBubble.setAttribute('class', 'divsContent');
+
+    const bubbleText = document.createElement('p');
+    bubbleText.innerHTML = "Алгоритм состоит в повторяющихся проходах по сортируемому массиву. На каждой итерации последовательно сравниваются соседние элементы, и, если порядок в паре неверный, то элементы меняют местами. За каждый проход по массиву как минимум один элемент встает на свое место, поэтому необходимо совершить не более n-1 проходов, где n nразмер массива, чтобы отсортировать массив."
+    bubbleText.setAttribute('class','algText');
+
+    const codeBubble = document.createElement('code');
+    codeBubble.setAttribute('class', 'codeText');
+    codeBubble.innerHTML = 'function bubble(array) {\
+        const len = array.length;\
+        for (let i = 1; i < len; i++) {\
+          for (let j = 0; j < len - i; j++) {\
+            if (array[j] > array[j + 1]) {\
+              [array[j], array[j + 1]] = [array[j + 1], array[j]];\
+            }\
+          }\
+        }\
+    }'
+    divContentBubble.append(bubbleText);
+    divContentBubble.append(codeBubble);
+
+    let tableBubble = document.createElement('table');
+    tableBubble.setAttribute('class', 'tableO')
+    let rowFirst = tableBubble.insertRow();
+    let titleTime = rowFirst.insertCell();
+    let titleBest = rowFirst.insertCell();
+    let titleMid = rowFirst.insertCell();
+    let titleWorst = rowFirst.insertCell();
+    titleTime.innerHTML = 'Время'
+    titleBest.innerHTML = 'Лучшее'
+    titleMid.innerHTML = 'Среднее'
+    titleWorst.innerHTML = 'Худшее'
+    let rowSecond = tableBubble.insertRow();
+    let cell5 = rowSecond.insertCell();
+    let cell6 = rowSecond.insertCell();
+    let cell7 = rowSecond.insertCell();
+    let cell8 = rowSecond.insertCell();
+    cell5.innerHTML = 'Bubble'
+    cell6.innerHTML = 'O(n)'
+    cell7.innerHTML = 'O(n^2)'
+    cell8.innerHTML = 'O(n^2)'
+    
+    sliderChild1.append(divContentBubble);
+    sliderChild1.append(tableBubble);
 
     sliderParent.append(sliderChild1);
     sliderParent.append(sliderChild2);
@@ -59,10 +112,10 @@ const algorithmsApp = (input) => {
 
     let offset = 0;
     btnPrev.addEventListener('click', function(event) {
-        if (offset < 600) {
+        if (offset < 800) {
             return
         }
-        offset -= 600;
+        offset -= 800;
         sliderParent.scrollTo({
             left: offset,
             behavior: 'smooth'
@@ -70,10 +123,10 @@ const algorithmsApp = (input) => {
     });
 
     btnNext.addEventListener('click', function(event) {
-        if (offset >= 1200) {
+        if (offset >= 1600) {
             return
         }
-        offset += 600;
+        offset += 800;
         sliderParent.scrollTo({
             left: offset,
             behavior: 'smooth'
