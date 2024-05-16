17568540c7d3cf64f095a741c4cdcfe35d34327f
Максим Дубинин <maksimdubinin@MacBook-Pro-Maksim.local>
2024-05-16 13:44:11 +0300

    добавил шейкерную сортировку

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ const algorithmsApp = (input) => {
     bubbleTextName.innerHTML = "Сортировка Пузырьком";
     divTitleBubble.append(bubbleTextName);
     const realizationText = document.createElement('p');
-    realizationText.setAttribute('id', 'realizationText');
+    realizationText.setAttribute('class', 'realizationText');
     realizationText.innerHTML = "Реализация на JS";
     divTitleBubble.append(realizationText);
     sliderChild1.append(divTitleBubble);
@@ -68,15 +68,16 @@ const algorithmsApp = (input) => {
 
     const codeBubble = document.createElement('code');
     codeBubble.setAttribute('class', 'codeText');
-    codeBubble.innerHTML = 'function bubble(array) {\
-        const len = array.length;\
-        for (let i = 1; i < len; i++) {\
-          for (let j = 0; j < len - i; j++) {\
-            if (array[j] > array[j + 1]) {\
-              [array[j], array[j + 1]] = [array[j + 1], array[j]];\
-            }\
-          }\
-        }\
+    codeBubble.innerHTML = '\
+    function bubble(array) {<br>\
+        <span class="t1"></span>const len = array.length;<br>\
+        <span class="t1"></span>for (let i = 1; i < len; i++) {<br>\
+            <span class="t2"></span>for (let j = 0; j < len - i; j++) {<br>\
+                <span class="t3"></span>if (array[j] > array[j + 1]) {<br>\
+                    <span class="t4"></span>[array[j], array[j + 1]] = [array[j + 1], array[j]];<br>\
+                    <span class="t3"></span>}<br>\
+                    <span class="t2"></span>}<br>\
+        <span class="t1"></span>}<br>\
     }'
     divContentBubble.append(bubbleText);
     divContentBubble.append(codeBubble);
@@ -93,17 +94,97 @@ const algorithmsApp = (input) => {
     titleMid.innerHTML = 'Среднее'
     titleWorst.innerHTML = 'Худшее'
     let rowSecond = tableBubble.insertRow();
-    let cell5 = rowSecond.insertCell();
-    let cell6 = rowSecond.insertCell();
-    let cell7 = rowSecond.insertCell();
-    let cell8 = rowSecond.insertCell();
-    cell5.innerHTML = 'Bubble'
-    cell6.innerHTML = 'O(n)'
-    cell7.innerHTML = 'O(n^2)'
-    cell8.innerHTML = 'O(n^2)'
+    let nameBubble = rowSecond.insertCell();
+    let firstTimeBubble = rowSecond.insertCell();
+    let secondTimeBubble = rowSecond.insertCell();
+    let thirdTimeBubble = rowSecond.insertCell();
+    nameBubble.innerHTML = 'Bubble'
+    firstTimeBubble.innerHTML = 'O(n)'
+    secondTimeBubble.innerHTML = 'O(n^2)'
+    thirdTimeBubble.innerHTML = 'O(n^2)'
     
     sliderChild1.append(divContentBubble);
     sliderChild1.append(tableBubble);
+
+
+    const divTitleShaker = document.createElement('div');
+    divTitleShaker.setAttribute('class', 'divsTitle');
+
+    const shakerTextName = document.createElement('p');
+    shakerTextName.innerHTML = 'Шейкерная сортировка'
+    shakerTextName.setAttribute('class', 'algName');
+
+    const realizationText2 = document.createElement('p');
+    realizationText2.setAttribute('class', 'realizationText');
+    realizationText2.innerHTML = "Реализация на JS";
+
+    divTitleShaker.append(shakerTextName);
+    divTitleShaker.append(realizationText2);
+
+    sliderChild2.append(divTitleShaker);
+
+    const divContentShaker = document.createElement('div');
+    divContentShaker.setAttribute('class','divsContent');
+
+    const shakerText = document.createElement('div');
+    shakerText.innerHTML = "Производится многократный прогон по массиву, соседние элементы сравниваются и, в случае необходимости, меняются местами. При достижении конца массива направление меняется на противоположное. Таким образом по очереди выталкиваются крупные и мелкие элементы массива в конец и начало структуры соответственно.";
+    shakerText.setAttribute('class', 'algText')
+
+    const codeShaker = document.createElement('code');
+    codeShaker.innerHTML = '\
+    function Shaker(array) {<br>\
+        <span class="t1"></span>for (let i = 0; i < Math.floor(array.length / 2); i++) {<br>\
+            <span class="t2"></span>let swapFlag = false;<br>\
+            <span class="t2"></span>for (let j = i; j < array.length - i - 1; j++) {<br>\
+                <span class="t3"></span>if (array[j] > array[j + 1]) {<br>\
+                    <span class="t4"></span>Swap(array, j, j + 1);<br>\
+                    <span class="t4"></span>swapFlag = true;<br>\
+                    <span class="t3"></span>}<br>\
+                    <span class="t2"></span>}<br>\
+                    <span class="t2"></span>for (let j = array.length - 2 - i; j > i; j--) {<br>\
+                        <span class="t3"></span>if (array[j - 1] > array[j]) {<br>\
+                            <span class="t4"></span>Swap(array, j - 1, j);<br>\
+                            <span class="t4"></span>swapFlag = true;<br>\
+                            <span class="t3"></span>}<br>\
+                            <span class="t2"></span>}<br>\
+                            <span class="t2"></span>if (!swapFlag) {<br>\
+                                <span class="t3"></span>break;<br>\
+                                <span class="t2"></span>}<br>\
+                                <span class="t1"></span>}<br>\
+      }' 
+    codeShaker.setAttribute('class', 'codeText');
+    divContentShaker.append(shakerText);
+    divContentShaker.append(codeShaker);
+
+    sliderChild2.append(divContentShaker);
+
+    const tableShaker = document.createElement('table');
+    tableShaker.setAttribute('class', 'tableO')
+    let rowFirstShaker = tableShaker.insertRow();
+    let titleTimeShaker = rowFirstShaker.insertCell();
+    let titleBestShaker = rowFirstShaker.insertCell();
+    let titleMidShaker = rowFirstShaker.insertCell();
+    let titleWorstShaker = rowFirstShaker.insertCell();
+
+    titleTimeShaker.innerHTML = 'Время'
+    titleBestShaker.innerHTML = 'Лучшее'
+    titleMidShaker.innerHTML = 'Среднее'
+    titleWorstShaker.innerHTML = 'Худшее'
+
+    let rowSecondShaker = tableShaker.insertRow();
+    let nameShaker = rowSecondShaker.insertCell();
+    let firstTimeShaker = rowSecondShaker.insertCell();
+    let secondTimeShaker = rowSecondShaker.insertCell();
+    let thirdTimeShaker = rowSecondShaker.insertCell();
+
+    nameShaker.innerHTML = 'Shaker'
+    firstTimeShaker.innerHTML = 'O(n)'
+    secondTimeShaker.innerHTML = 'O(n^2)'
+    thirdTimeShaker.innerHTML = 'O(n^2)'
+
+
+    sliderChild2.append(tableShaker);
+
 
     sliderParent.append(sliderChild1);
     sliderParent.append(sliderChild2);
